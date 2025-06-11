fe789f30f3b0631793c3391080fb69ea24ad4adc
bluVshal <blu.vishal@gmail.com>
2025-06-11 10:22:32 +0400

    Added lib file for functions and implemented search all cars

--- app/models/cars.py
+++ app/models/cars.py
@@ -1,8 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import LargeBinary
+import enum
 
-db=SQLAlchemy()
-class Car(db.Model):
+
+class CarStatusEnum(enum.Enum):
+    Available = 'Available'
+    Rented = 'Rented'
+    Maintenance = 'Maintenance'
+    Sold = 'Sold'
+
+
+class CarTypeEnum(enum.Enum):
+    Small = 'Small'
+    Compact = 'Compact'
+    Large = 'Large'
+    Limousine = 'Limousine'
+
+
+class CarTransmissionTypeEnum(enum.Enum):
+    AT = 'AT'
+    MT = 'MT'
+
+
+db = SQLAlchemy()
+
+
+class Cars(db.Model):
     carId = db.Column(db.Integer, unique=True, primary_key=True)
     carChassisNumber = db.Column(db.String(17), unique=True, nullable=False)
     carMake = db.Column(db.String(100), nullable=False)
@@ -10,17 +34,17 @@ class Car(db.Model):
     carNTARegNumber = db.Column(db.String(45), nullable=False)
     carBuyDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     carPrice = db.Column(db.Text, nullable=False)
-    carStatus
+    carStatus = db.Column(db.Enum(CarStatusEnum), nullable=False)
     carSoldDate = db.Column(db.DateTime, default=datetime.utcnow)
-    carPhoto
-    carType
-    isCarElectric
-    isCarHybrid
-    carTransmission
-    carSeats
-    carNoDoors
-    carHP
-    carCo2Emission
-    active
+    carPhoto = db.Column(LargeBinary)
+    carType = db.Column(db.Enum(CarTypeEnum), nullable=False)
+    isCarElectric = db.Column(db.Boolean(), default=False)
+    isCarHybrid = db.Column(db.Boolean(), default=False)
+    carTransmission = db.Column(db.Enum(CarTransmissionTypeEnum), nullable=False)
+    carSeats = db.Column(db.Integer, nullable=False)
+    carNoDoors = db.Column(db.Integer, nullable=False)
+    carHP = db.Column(db.Integer, nullable=False)
+    carCo2Emission = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Boolean(), default=False)
     carAddDate = db.Column(db.DateTime, default=datetime.utcnow)
     carLastModDate = db.Column(db.DateTime, default=datetime.utcnow)
--- server.py
+++ server.py
@@ -1,21 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from dotenv import load_dotenv
+from app.models.cars import db, Cars
+
+
 load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQL_ALCHEMY_DATABASE_URI'] = (
+app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
     f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 )
 
-app.config['JWT_SECRET_KEY']=os.getenv('JWT_SECRET')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
+
+db.init_app(app)
+
+@app.get('/cars')
+def carsRouteAccess():
+    return 'Inside Cars'
+
+
+@app.get('/cars/all')
+def getAllCars():
+    cars = Cars.query.all()
+    for car in cars:
+        print(car.carChassisNumber)
+    return 'cars'
+
 
 @app.get('/')
 def index():  # put application's code here
-    return 'Hello World!'
+    return 'Welcome to NuAuto!'
 
 
 if __name__ == '__main__':
     app.run()
+
