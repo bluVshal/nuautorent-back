1618bfd80d0fb36689c771f6bd01a2432a564082
bluVshal <blu.vishal@gmail.com>
2025-06-12 16:33:47 +0400

    Added CORS issue handler

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import os
 from dotenv import load_dotenv
 from app.models.cars import db, Cars
 from app.utils.lib import to_dict
-
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
